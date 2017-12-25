47d783b13307382d8176942eefcf611ddd9c61c5
John <fun@Johns-MacBook-Air.local>
2017-12-25 13:29:30 -0500

    nothing

--- client-side/src/app/Caliber/caliber.module.ts
+++ client-side/src/app/Caliber/caliber.module.ts
@@ -1,27 +1,46 @@
+// modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpModule } from '@angular/http';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { RouterModule } from '@angular/router';
 import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
+import { NavModule } from '../nav/nav.module';
+
+// routing
 import { routes } from './caliber.routes';
+import { SpringInterceptor } from './interceptors/spring.interceptor';
+
+// services
+import { BatchService } from './services/batch.service';
+import { TrainerService } from './services/trainer.service';
+import { TraineeService } from './services/trainee.service';
+import { EnvironmentService } from './services/environment.service';
+import { AssessmentService } from './services/assessment.service';
+import { RouteService } from './services/route.service';
+import { PanelService } from './services/panel.service';
+import { GradeService } from './services/grade.service';
+
+// components
 import { CaliberComponent } from './caliber.component';
 import { HomeComponent } from './home/home.component';
 import { AssessComponent } from './assess/assess.component';
 import { NavComponent } from '../nav/nav.component';
-import { NavModule } from '../nav/nav.module';
 import { ManageComponent } from './manage/manage.component';
 import { ReportsComponent } from './reports/reports.component';
-import { CommonModule } from '@angular/common';
 import { WeeklyLineChartComponent } from './weekly-line-chart/weekly-line-chart.component';
 import { TrainerProfileComponent } from './trainer-profile/trainer-profile.component';
 import { TrainerService } from './services/trainer-service/trainer-service.service';
+import { TestComponent } from './components/test/test.component';
 
 
 @NgModule({
   imports: [
     CommonModule,
     HttpModule,
+    HttpClientModule,
     NgbModule.forRoot(),
     RouterModule.forChild(routes),
     FormsModule,
@@ -33,10 +52,28 @@ import { TrainerService } from './services/trainer-service/trainer-service.servi
     ManageComponent,
     ReportsComponent,
     WeeklyLineChartComponent,
+<<<<<<< HEAD
     TrainerProfileComponent,
   ],
   providers: [
     TrainerService
   ]
+=======
+    TestComponent,
+  ],
+  providers: [
+    { provide: HTTP_INTERCEPTORS, useClass: SpringInterceptor, multi: true },  // interceptor for all HTTP requests
+    BatchService,
+    EnvironmentService,
+    TrainerService,
+    TraineeService,
+    AssessmentService,
+    RouteService,
+    PanelService,
+    RouteService,
+    GradeService,
+
+  ],
+>>>>>>> trainer-profile
 })
 export class CaliberModule { }
--- client-side/src/app/Caliber/components/test/test.component.spec.ts
+++ client-side/src/app/Caliber/components/test/test.component.spec.ts
@@ -1,20 +1,20 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { DashboardComponent } from './dashboard.component';
+import { TestComponent } from './test.component';
 
-describe('DashboardComponent', () => {
-  let component: DashboardComponent;
-  let fixture: ComponentFixture<DashboardComponent>;
+describe('TestComponent', () => {
+  let component: TestComponent;
+  let fixture: ComponentFixture<TestComponent>;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ DashboardComponent ]
+      declarations: [ TestComponent ]
     })
     .compileComponents();
   }));
 
   beforeEach(() => {
-    fixture = TestBed.createComponent(DashboardComponent);
+    fixture = TestBed.createComponent(TestComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
